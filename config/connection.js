03c4952c5e2826912e51a5c01256b9fc9026fba3
Turan Ketene <turank97@gmail.com>
2019-10-30 16:22:27 -0700

    added jawsdb url to connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,10 +1,14 @@
 var mysql = require("mysql");
-var connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'password',
-    database: 'burgers_db'
-})
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else{
+    var connection = mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: 'password',
+        database: 'burgers_db'
+    })
+}
 connection.connect(function(err) {
     if(err) throw err;
     console.log("connected as id: "+ connection.threadId);
