d511be21a551f7f76cef0f2b68faf588450402e4
Turan Ketene <turank97@gmail.com>
2019-10-30 21:32:46 -0700

    fixed bug with connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,8 +1,9 @@
 var mysql = require("mysql");
+var connection;
 if (process.env.JAWSDB_URL) {
     connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else{
-    var connection = mysql.createConnection({
+        connection = mysql.createConnection({
         host: 'localhost',
         user: 'root',
         password: 'password',
